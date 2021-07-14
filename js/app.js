f78608b5507da17d389a8b78bdfa549a430fe084
unknown <olivierakesso@gmail.com>
2021-07-14 15:12:41 +0200

    code modifications

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-var canvas = document.getElementById("canvasOne");
-canvas.addEventListener('keydown', actionOnArrowKeys, true);
-const ctx = canvas.getContext('2d');
+// -------------------------------------------------------
+// Canvas One treatments.
+// -------------------------------------------------------
+
+let canvasOne = document.getElementById("canvasOne");
+canvasOne.addEventListener('keydown', actionOnArrowKeys, true);
+const ctx1 = canvasOne.getContext('2d');
+
 let x = 250; // starting position on x axis
 let y = 0; // starting position on y axis
 let vx = 50; // speed applied for x axis moves.
 let vy = 50; // speed applied for y axis moves.
-let color = 'blue';
+let color = 'rgba(185, 211, 238, 0.7)';
 const h = 50; // hight size.
 const l = h; // width size (= h to have a square shape.
 const movement = 50; //  distance applied on arrow key pressed.
+let limitHeight = canvasOne.height - h;
+let limitWidth = canvasOne.width - h;
 
 // square limit for top side. 
 function topWall () {
@@ -19,12 +26,10 @@ function topWall () {
   }
 }
 
-let limit = canvas.height - h;
-
 // square limit for bottom side. 
 function bottomWall () {
-  if (y >= limit) {
-    y = limit;
+  if (y >= limitHeight) {
+    y = limitHeight;
   } else {
     y = y + movement;    
   }
@@ -41,8 +46,8 @@ function leftWall () {
 
 //square shape limit for rigth side.
 function rigthWall () {
-  if (x >= (canvas.width - h)) {
-    x = (canvas.width - h);
+  if (x >= limitWidth) {
+    x = limitWidth;
   } else {
     x = x + movement;    
   }
@@ -51,49 +56,55 @@ function rigthWall () {
 function actionOnArrowKeys(e){
   // UP.
   if (e.keyCode === 38) {
-    if (y !== (canvas.height - h)) {
+    if (y !== limitHeight) {
       topWall ();
     }
     
   }
   // DOWN.
   if (e.keyCode === 40) {
-    if (y !== (canvas.height - h)) {
+    if (y !== limitHeight) {
       bottomWall ();
     }
   }
   // LEFT.
   if (e.keyCode === 37) {
-    if (y !== (canvas.height - 50)) {
+    if (y !== limitHeight) {
       leftWall (); 
     }   
   }
   //	RIGHT.
   if (e.keyCode === 39) {
-    if (y !== (canvas.height - 50)) {
+    if (y !== limitHeight) {
       rigthWall ();    
     }
   }
 }
 
-function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'rgba(185, 211, 238, 0.7)'; 
-  ctx.fillRect(x, y, h, l);
-  // ctx.fillRect(50, 50, 100, l);  
+function elements () {
+  ctx1.fillRect(100, 300, 150, 50);
+  ctx1.fillRect(100, 300, 150, 50);
+}
+
+
+function firstElement() {
+  ctx1.clearRect(0, 0, canvasOne.width, canvasOne.height);
+  ctx1.fillStyle = color; 
+  ctx1.fillRect(x, y, h, l);
+  elements();  
   x += 0; // don't move here but to moves on x axis put "vx". 
   y += vy; // up & down moves moves on y axis.
-  if (y + vy > canvas.height - h || y + vy < 0) {
+  if (y + vy > limitHeight || y + vy < 0) {
     vy *= 0; // put -1 here to rebound on contact with a side.
-    console.log(x, y); // print shape's coordinates
   }
   // condition for width ==> collide and rebound for right & left side.
-  if (x + vx > canvas.width - h  || x + vx < 0) {
+  if (x + vx > limitWidth  || x + vx < 0) {
     vx *= -1;
   }
 }
 
-setInterval(update, 1000);
+setInterval(firstElement, 800);
+
 
 
 
